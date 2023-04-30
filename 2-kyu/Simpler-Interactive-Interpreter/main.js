3dda9618f02cce41241dd76cbb6848776f3ce223
tomiis4 <87276646+tomiis4@users.noreply.github.com>
2023-04-30 22:12:17 +0200

    Finishing eval function

--- 2-kyu/Simpler-Interactive-Interpreter/main.js
+++ 2-kyu/Simpler-Interactive-Interpreter/main.js
@@ -11,11 +11,18 @@ class Interpreter {
 			const value = this.getValues(expr.replace(`${kw[0]} = `, ''));
 
 			if (value.split(' ').length == 1) {
-				this.vars[kw[0]] = value;
-				return value;
+				this.vars[kw[0]] = parseFloat(value);
+				return parseFloat(value);
 			} else {
 				this.vars[kw[0]] = this.eval(value);
 				return this.eval(value);
+			}
+		}
+		if (expr.replace(/[a-zA-Z]/g, '') == '') {
+			if (this.vars[kw[0]]) {
+				return this.vars[kw[0]]
+			} else {
+				throw new Error(`ERROR: Invalid identifier. No variable with name '${expr}' was found."`)
 			}
 		}
 		if (!expr.includes('=')) {
@@ -33,130 +40,157 @@ class Interpreter {
 		}).join('')
 	}
 
-	eval(expr) {
-	// leave there result without groups
-	let resultStr = expr.split('').join('');
-	// store groups
+eval(expr) {
 	let groups = [];
 
-	// split by groups and to result add GROUPS_ID
+	// get groups
 	const groupRegex = /\(([^()]*)\)/g;
-	while (resultStr.match(groupRegex) != null) {
-		let splitGroups = resultStr.match(groupRegex)
+	while (expr.match(groupRegex) != null) {
+		let splitGroups = expr.match(groupRegex)
 
 		groups.push(...splitGroups)
-		resultStr = resultStr.replace(groupRegex, `GROUP_${groups.length-1}`)
+		expr = expr.replace(groupRegex, `GROUP_${groups.length-1}`)
 	}
 
 	// calculate groups
-	groups.forEach((group, i) => {
-		// if current group contain another group, add it
-		// remove ()
-		group = group.replace(/GROUP_(\d+)/g, (_, gIdx) => groups[gIdx])
-		group = group.replace(/\(|\)/gm, '');
-
-		// calculate result of group, split by * to calculatoe first +-%
-		// TODO: add division
-		let groupsResults = group.split(' * ').map(beforeMultiply => {
-			let multiplyGroupResult = 0;
-
-			// calculate +- in groups splitted by *
-			beforeMultiply.split(' ').forEach((operator, operatorIdx) => {
-				// get num beforeOperator & afterOperator
-				const a = parseInt(beforeMultiply.split(' ')[operatorIdx-1])
-				const b = parseInt(beforeMultiply.split(' ')[operatorIdx+1])
-
-				// if is current character operator
-				if (isNaN(parseInt(operator))) {
-					// if is first operator, work with first number 
-					// else work with prev. result
-					if (operatorIdx == 1) {
-						multiplyGroupResult = this.calc(a, b, operator)
-					} else {
-						multiplyGroupResult = this.calc(multiplyGroupResult, b, operator)
-					}
-				}
-			});
-
-			return multiplyGroupResult
-		}).join(' * ');
-
-		let tempMultiplied = 1;
-		groupsResults.split(' * ').forEach((el) => {
-			tempMultiplied *= parseInt(el);
-		})
-
-		group = tempMultiplied
-		groups[i] = group
-	});
-
-	// replace groups to numbers
-	resultStr = resultStr.replace(/GROUP_(\d+)/g, (_, gIdx) => groups[gIdx])
-
-	// multiply divide
-	let tempMultiply = 0;
-	let remove = [];
-	resultStr.split(' ').forEach((element, index) => {
-		const a = resultStr.split(' ')[index-1]
-		const b = resultStr.split(' ')[index+1]
-
-		if (element == "*") {
-			tempMultiply += a*b;
-			remove.push(index-1, index, index+1)
+	groups = groups.map(group => {
+		if (group.includes('GROUP_')) {
+			group = group.replace(/GROUP_(\d+)/g, (_, groupCapture) => groups[groupCapture]);
 		}
-		else if (element == "/") {
-			tempMultiply += a/b;
-			remove.push(index-1, index, index+1)
-		}
-	});
 
-	let multipliedArr = [tempMultiply.toString(), ...resultStr.split(' ').filter((_, index) => {
-		return !remove.includes(index)
-	})].join(' ');
+		group = group.replace(/\(|\)/gm, ''); // remove ()
+		group = this.calculate_expr(group)
 
-	if (multipliedArr.split(' ').length == 1) return parseInt(multipliedArr)
+		return group;
+	})
 
-	// +-%
-	let result = 0
-	multipliedArr.split(' ').forEach((operator, operatorIdx) => {
-		const a = parseInt(multipliedArr.split(' ')[operatorIdx-1])
-		const b = parseInt(multipliedArr.split(' ')[operatorIdx+1])
 
-		// if is current character operator
-		if (isNaN(parseInt(operator))) {
-		// if is first operator, work with first number 
-		// else work with prev. result
-			if (operatorIdx == 1) {
-				result = this.calc(a, b, operator)
-			} else {
-				result = this.calc(result, b, operator)
-			}
-		}
-	});
+	// insert groups to main
+	if (expr.includes('GROUP_')) {
+		expr = expr.replace(/GROUP_(\d+)/g, (_, groupCapture) => groups[groupCapture]);
+	}
 
-	return result
+	expr = this.calculate_expr(expr)
+
+	console.log(expr, groups)
 }
-calc(a, b, o) {
-   switch (o) {
-		case '+':
-			return parseInt(a) + parseInt(b)
-		case '-':
-			return parseInt(a) - parseInt(b)
-		case '*':
-		return parseInt(a) * parseInt(b)
-		case '/':
-			return parseInt(a) / parseInt(b)
-		case '%':
-			return parseInt(a) % parseInt(b)
-		default: throw new Error(`Invalid operator ${o}`)
-   }
+
+calculate_expr(expr) {
+	let removeIdxs = [];
+
+	// *
+	if (expr.includes('*')) {
+		expr = expr.split(' ').map((nums, numIdx) => {
+			if (nums == "*") {
+				const a = parseFloat(expr.split(' ')[numIdx - 1])
+				const b = parseFloat(expr.split(' ')[numIdx + 1])
+
+				removeIdxs.push(numIdx-1, numIdx+1)
+				return a * b
+			} else {
+				return nums
+			}
+		}).join(' ');
+	}
+
+	// remove indexes
+	expr = expr.split(' ').filter((_, exprIdx) => {
+		return !removeIdxs.includes(exprIdx);
+	}).join(' ');
+	removeIdxs = [];
+
+	// /
+	if (expr.includes('/')) {
+		expr = expr.split(' ').map((nums, numIdx) => {
+			if (nums == "/") {
+				const a = parseFloat(expr.split(' ')[numIdx - 1])
+				const b = parseFloat(expr.split(' ')[numIdx + 1])
+
+				removeIdxs.push(numIdx-1, numIdx+1)
+				return a / b
+			} else {
+				return nums
+			}
+		}).join(' ');
+	}
+
+	// remove indexes
+	expr = expr.split(' ').filter((_, exprIdx) => {
+		return !removeIdxs.includes(exprIdx);
+	}).join(' ');
+	removeIdxs = [];
+
+	// +
+	if (expr.includes('+')) {
+		expr = expr.split(' ').map((nums, numIdx) => {
+			if (nums == "+") {
+				const a = parseFloat(expr.split(' ')[numIdx - 1])
+				const b = parseFloat(expr.split(' ')[numIdx + 1])
+
+				removeIdxs.push(numIdx-1, numIdx+1)
+				return a + b
+			} else {
+				return nums
+			}
+		}).join(' ');
+	}
+	// remove indexes
+	expr = expr.split(' ').filter((_, exprIdx) => {
+		return !removeIdxs.includes(exprIdx);
+	}).join(' ');
+	removeIdxs = [];
+
+	// -
+	if (expr.includes('-')) {
+		expr = expr.split(' ').map((nums, numIdx) => {
+			if (nums == "-") {
+				const a = parseFloat(expr.split(' ')[numIdx - 1])
+				const b = parseFloat(expr.split(' ')[numIdx + 1])
+
+				removeIdxs.push(numIdx-1, numIdx+1)
+				return a - b
+			} else {
+				return nums
+			}
+		}).join(' ');
+	}
+	// remove indexes
+	expr = expr.split(' ').filter((_, exprIdx) => {
+		return !removeIdxs.includes(exprIdx);
+	}).join(' ');
+	removeIdxs = [];
+
+	// %
+	if (expr.includes('%')) {
+		expr = expr.split(' ').map((nums, numIdx) => {
+			if (nums == "%") {
+				const a = parseFloat(expr.split(' ')[numIdx - 1])
+				const b = parseFloat(expr.split(' ')[numIdx + 1])
+
+				removeIdxs.push(numIdx-1, numIdx+1)
+				return a % b
+			} else {
+				return nums
+			}
+		}).join(' ');
+	}
+	// remove indexes
+	expr = expr.split(' ').filter((_, exprIdx) => {
+		return !removeIdxs.includes(exprIdx);
+	}).join(' ');
+	removeIdxs = [];
+
+	return parseFloat(expr)
 }
 }
 
 
 let comp = new Interpreter
 // console.log(comp.input('x = (8 - (4 + 2)) * 3'))
-console.log(comp.input('x = 1'))
+// console.log(comp.input('x = 4'))
+// console.log(comp.input('x'))
+// console.log(comp.input('x + 3'))
+// console.log(comp.input('2 * 3'))
 // console.log(comp.input('(8 - (4 + 2)) * 3'))
 
 
@@ -166,123 +200,162 @@ console.log(comp.input('x = 1'))
 
 
 function eval(expr) {
-	// leave there result without groups
-	let resultStr = expr.split('').join('');
-	// store groups
 	let groups = [];
 
-	// split by groups and to result add GROUPS_ID
+	// get groups
 	const groupRegex = /\(([^()]*)\)/g;
-	while (resultStr.match(groupRegex) != null) {
-		let splitGroups = resultStr.match(groupRegex)
+	while (expr.match(groupRegex) != null) {
+		let splitGroups = expr.match(groupRegex)
 
 		groups.push(...splitGroups)
-		resultStr = resultStr.replace(groupRegex, `GROUP_${groups.length-1}`)
+		expr = expr.replace(groupRegex, `GROUP_${groups.length-1}`)
 	}
 
 	// calculate groups
-	groups.forEach((group, i) => {
-		// if current group contain another group, add it
-		// remove ()
-		group = group.replace(/GROUP_(\d+)/g, (_, gIdx) => groups[gIdx])
-		group = group.replace(/\(|\)/gm, '');
-
-		// calculate result of group, split by * to calculatoe first +-%
-		// TODO: add division
-		let groupsResults = group.split(' * ').map(beforeMultiply => {
-			let multiplyGroupResult = 0;
-
-			// calculate +- in groups splitted by *
-			beforeMultiply.split(' ').forEach((operator, operatorIdx) => {
-				// get num beforeOperator & afterOperator
-				const a = parseInt(beforeMultiply.split(' ')[operatorIdx-1])
-				const b = parseInt(beforeMultiply.split(' ')[operatorIdx+1])
-
-				// if is current character operator
-				if (isNaN(parseInt(operator))) {
-					// if is first operator, work with first number 
-					// else work with prev. result
-					if (operatorIdx == 1) {
-						multiplyGroupResult = calc(a, b, operator)
-					} else {
-						multiplyGroupResult = calc(multiplyGroupResult, b, operator)
-					}
-				}
-			});
-
-			return multiplyGroupResult
-		}).join(' * ');
-
-		let tempMultiplied = 1;
-		groupsResults.split(' * ').forEach((el) => {
-			tempMultiplied *= parseInt(el);
-		})
-
-		group = tempMultiplied
-		groups[i] = group
-	});
-
-	// replace groups to numbers
-	resultStr = resultStr.replace(/GROUP_(\d+)/g, (_, gIdx) => groups[gIdx])
-
-	// multiply divide
-	let tempMultiply = 0;
-	let remove = [];
-	resultStr.split(' ').forEach((element, index) => {
-		const a = resultStr.split(' ')[index-1]
-		const b = resultStr.split(' ')[index+1]
-
-		if (element == "*") {
-			tempMultiply += a*b;
-			remove.push(index-1, index, index+1)
+	groups = groups.map(group => {
+		if (group.includes('GROUP_')) {
+			group = group.replace(/GROUP_(\d+)/g, (_, groupCapture) => groups[groupCapture]);
 		}
-		else if (element == "/") {
-			tempMultiply += a/b;
-			remove.push(index-1, index, index+1)
-		}
-	});
 
-	let multipliedArr = [tempMultiply.toString(), ...resultStr.split(' ').filter((_, index) => {
-		return !remove.includes(index)
-	})].join(' ');
+		group = group.replace(/\(|\)/gm, ''); // remove ()
+		group = calculate_expr(group)
 
-	// +-%
-	let result = 0
-	multipliedArr.split(' ').forEach((operator, operatorIdx) => {
-		const a = parseInt(multipliedArr.split(' ')[operatorIdx-1])
-		const b = parseInt(multipliedArr.split(' ')[operatorIdx+1])
+		return group;
+	})
 
-		// if is current character operator
-		if (isNaN(parseInt(operator))) {
-		// if is first operator, work with first number 
-		// else work with prev. result
-			if (operatorIdx == 1) {
-				result = calc(a, b, operator)
+
+	// insert groups to main
+	if (expr.includes('GROUP_')) {
+		expr = expr.replace(/GROUP_(\d+)/g, (_, groupCapture) => groups[groupCapture]);
+	}
+
+	expr = calculate_expr(expr)
+
+	console.log(expr, groups)
+}
+
+function calculate_expr(expr) {
+	let removeIdxs = [];
+
+	// *
+	if (expr.includes('*')) {
+		expr = expr.split(' ').map((nums, numIdx) => {
+			if (nums == "*") {
+				const a = parseFloat(expr.split(' ')[numIdx - 1])
+				const b = parseFloat(expr.split(' ')[numIdx + 1])
+
+				removeIdxs.push(numIdx-1, numIdx+1)
+				return a * b
 			} else {
-				result = calc(result, b, operator)
+				return nums
 			}
-		}
-	});
+		}).join(' ');
+	}
 
+	// remove indexes
+	expr = expr.split(' ').filter((_, exprIdx) => {
+		return !removeIdxs.includes(exprIdx);
+	}).join(' ');
+	removeIdxs = [];
 
+	// /
+	if (expr.includes('/')) {
+		expr = expr.split(' ').map((nums, numIdx) => {
+			if (nums == "/") {
+				const a = parseFloat(expr.split(' ')[numIdx - 1])
+				const b = parseFloat(expr.split(' ')[numIdx + 1])
 
-	return result
+				removeIdxs.push(numIdx-1, numIdx+1)
+				return a / b
+			} else {
+				return nums
+			}
+		}).join(' ');
+	}
+
+	// remove indexes
+	expr = expr.split(' ').filter((_, exprIdx) => {
+		return !removeIdxs.includes(exprIdx);
+	}).join(' ');
+	removeIdxs = [];
+
+	// +
+	if (expr.includes('+')) {
+		expr = expr.split(' ').map((nums, numIdx) => {
+			if (nums == "+") {
+				const a = parseFloat(expr.split(' ')[numIdx - 1])
+				const b = parseFloat(expr.split(' ')[numIdx + 1])
+
+				removeIdxs.push(numIdx-1, numIdx+1)
+				return a + b
+			} else {
+				return nums
+			}
+		}).join(' ');
+	}
+	// remove indexes
+	expr = expr.split(' ').filter((_, exprIdx) => {
+		return !removeIdxs.includes(exprIdx);
+	}).join(' ');
+	removeIdxs = [];
+
+	// -
+	if (expr.includes('-')) {
+		expr = expr.split(' ').map((nums, numIdx) => {
+			if (nums == "-") {
+				const a = parseFloat(expr.split(' ')[numIdx - 1])
+				const b = parseFloat(expr.split(' ')[numIdx + 1])
+
+				removeIdxs.push(numIdx-1, numIdx+1)
+				return a - b
+			} else {
+				return nums
+			}
+		}).join(' ');
+	}
+	// remove indexes
+	expr = expr.split(' ').filter((_, exprIdx) => {
+		return !removeIdxs.includes(exprIdx);
+	}).join(' ');
+	removeIdxs = [];
+
+	// %
+	if (expr.includes('%')) {
+		expr = expr.split(' ').map((nums, numIdx) => {
+			if (nums == "%") {
+				const a = parseFloat(expr.split(' ')[numIdx - 1])
+				const b = parseFloat(expr.split(' ')[numIdx + 1])
+
+				removeIdxs.push(numIdx-1, numIdx+1)
+				return a % b
+			} else {
+				return nums
+			}
+		}).join(' ');
+	}
+	// remove indexes
+	expr = expr.split(' ').filter((_, exprIdx) => {
+		return !removeIdxs.includes(exprIdx);
+	}).join(' ');
+	removeIdxs = [];
+
+	return parseFloat(expr)
 }
 
-function calc(a, b, o) {
-   switch (o) {
-		case '+':
-			return parseInt(a) + parseInt(b)
-		case '-':
-			return parseInt(a) - parseInt(b)
-		case '*':
-		return parseInt(a) * parseInt(b)
-		case '/':
-			return parseInt(a) / parseInt(b)
-		case '%':
-			return parseInt(a) % parseInt(b)
-		default: throw new Error(`Invalid operator ${o}`)
-   }
-}
+// FIXME
+//	2+2 => 4 (2)
+//	4-6 => -2 (4)
+//
+// 1) 4 / 2 * 3 => 6 (0.666)
+// 2) (7 + 3) / (2 * 2 + 1) => 2 (1)
+// 3) (10 / (8 - (4 + 2))) * 3 => 15 (NaN)
 
-eval('(8 - (4 + 2)) * 3 + 1')
+
+// calculate_expr('5 * 2 / 3') // 3.33
+// calculate_expr('6 / 3 * 2 - 10') // -6 
+// eval('(8 - (4 + 2)) * 3 + 1') // 7
+// eval('5') // 5
+// eval('2 * 3') // 6
+// eval('6 / 3') // 2
+// eval('6 + 3') // 9
+// eval('2 / 0') // ERROR
